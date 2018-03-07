ff380dc0f3427df67339fc5340ef86480e8631a9
BugBugGoAway <BugBugGoAway@163.com>
2018-03-07 16:26:43 +0800

    201803071626

--- damaiTicket.py
+++ damaiTicket.py
@@ -10,8 +10,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class DaMaiTicket(object):
 
-    username = "13701501927"
-    password = "19890605lb"
+    username = ""
+    password = ""
     url = "https://piao.damai.cn/143439.html?spm=a2o6e.search.0.0.2bd01e33evOkdz"
     count = 0
 
