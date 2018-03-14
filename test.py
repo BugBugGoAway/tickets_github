f42056465c488d7dd18292a43aa5200eeb56ca01
BugBugGoAway <BugBugGoAway@163.com>
2018-03-14 17:03:29 +0800

    201803141703

--- test.py
+++ test.py
@@ -1,32 +1,70 @@
 # -*- coding: UTF-8 -*-
 
-from aip import AipOcr
-import json
 
-# 定义常量
-APP_ID = '9851066'
-API_KEY = 'LUGBatgyRGoerR9FZbV4SQYk'
-SECRET_KEY = 'fB2MNz1c2UHLTximFlC4laXPg7CVfyjV'
-
-# 初始化AipFace对象
-aipOcr = AipOcr(APP_ID, API_KEY, SECRET_KEY)
-
-# 读取图片
-filePath = "C:/Users/Lee/Desktop/title.png"
-
-
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
-        return fp.read()
-
-    # 定义参数变量
+# 汉诺塔 a b c表示3个柱子，n表示柱子上的盘子
+def hannuota(n, a , b , c):
+    if n == 1:
+        print(a + "==>" + c)
+    # elif n == 2:
+    #     print(a, "==>", b)
+    #     print(a, "==>", c)
+    #     print(b, "==>", c)
+    else:
+        hannuota(n-1, a, c, b)
+        hannuota(1, a, b, c)
+        hannuota(n-1, b, a, c)
 
 
-options = {
-    'detect_direction': 'true',
-    'language_type': 'CHN_ENG',
-}
+ # 使用生成器打印斐波那契数列
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield(b)
+        a, b = b, a + b
+        # 以上语法糖写法等价于以下3行代码
+        # temp = a
+        # a = b
+        # b = temp + b
+        n = n + 1
+    return 'done'
 
-# 调用通用文字识别接口
-result = aipOcr.basicGeneral(get_file_content(filePath), options)
-print(json.dumps(result))
+
+# 生成器打印杨辉三角
+def yanghui_triangles(max):
+    line = 1
+    self = []
+    pre = []
+    while line <= max:
+        yield self
+        self.clear()
+        n = 1
+        while n <= line:
+            if n == 1 or n == line:
+                temp = 1
+            else:
+                temp = pre[n-2]+pre[n-1]
+            n = n+1
+            self.append(temp)
+        print(self)
+        pre.clear()
+        pre = self.copy()
+        line = line + 1
+
+
+fb = fib(10)
+
+yh = yanghui_triangles(10)
+
+for f in fb:
+    print(f)
+
+for t in yh:
+    print(t)
+
+hannuota(3,"A","B","C")
+
+
+
+
+
+
